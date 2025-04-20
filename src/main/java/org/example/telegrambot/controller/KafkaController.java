081f1626305f340b194a8e4552bf1218364e86c6
PetrosiunArtem <petrosiun.av@phystech.edu>
2025-04-20 04:19:51 +0300

    add KafkaController

---
+++ src/main/java/org/example/telegrambot/controller/KafkaController.java
@@ -0,0 +1,28 @@
+package org.example.telegrambot.controller;
+
+import org.example.telegrambot.kafka.KafkaListenerCreator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KafkaController {
+  @Autowired KafkaListenerCreator kafkaListenerCreator;
+  @Autowired private KafkaTemplate<String, String> kafkaTemplate;
+
+  @PostMapping(path = "/create")
+  @ResponseStatus(HttpStatus.OK)
+  public void create(@RequestParam String topic) {
+    kafkaListenerCreator.createAndRegisterListener(topic);
+  }
+
+  @PostMapping(path = "/send")
+  @ResponseStatus(HttpStatus.OK)
+  public void send(@RequestParam String topic, @RequestParam String message) {
+    kafkaTemplate.send(topic, message);
+  }
+}
---
+++ src/main/java/org/example/telegrambot/kafka/KafkaListenerCreator.java
@@ -0,0 +1,58 @@
+package org.example.telegrambot.kafka;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerEndpoint;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
+import org.springframework.kafka.config.MethodKafkaListenerEndpoint;
+import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+@RequiredArgsConstructor
+public class KafkaListenerCreator {
+  private final KafkaListenerEndpointRegistry kafkaListenerEndpointRegistry;
+  private final KafkaListenerContainerFactory kafkaListenerContainerFactory;
+
+  String kafkaGroupId = "kafkaGroupId";
+  String kafkaListenerId = "kafkaListenerId-";
+  static AtomicLong endpointIdIndex = new AtomicLong(1);
+
+  private KafkaListenerEndpoint createKafkaListenerEndpoint(String topic) {
+    MethodKafkaListenerEndpoint<String, String> kafkaListenerEndpoint =
+        createDefaultMethodKafkaListenerEndpoint(topic);
+    kafkaListenerEndpoint.setBean(new KafkaTemplateListener());
+    try {
+      kafkaListenerEndpoint.setMethod(
+          KafkaTemplateListener.class.getMethod("onMessage", ConsumerRecord.class));
+    } catch (NoSuchMethodException e) {
+      throw new RuntimeException("Attempt to call a non-existent method " + e);
+    }
+    return kafkaListenerEndpoint;
+  }
+
+  private MethodKafkaListenerEndpoint<String, String> createDefaultMethodKafkaListenerEndpoint(
+      String topic) {
+    MethodKafkaListenerEndpoint<String, String> kafkaListenerEndpoint =
+        new MethodKafkaListenerEndpoint<>();
+    kafkaListenerEndpoint.setId(generateListenerId());
+    kafkaListenerEndpoint.setGroupId(kafkaGroupId);
+    kafkaListenerEndpoint.setAutoStartup(true);
+    kafkaListenerEndpoint.setTopics(topic);
+    kafkaListenerEndpoint.setMessageHandlerMethodFactory(new DefaultMessageHandlerMethodFactory());
+    return kafkaListenerEndpoint;
+  }
+
+  private String generateListenerId() {
+    return kafkaListenerId + endpointIdIndex.getAndIncrement();
+  }
+
+  public void createAndRegisterListener(String topic) {
+    KafkaListenerEndpoint listener = createKafkaListenerEndpoint(topic);
+    kafkaListenerEndpointRegistry.registerListenerContainer(
+        listener, kafkaListenerContainerFactory, true);
+  }
+}
